396b17b1a94b9014b0238aabcde29070e6d97f41
server <server@TechnoCore.SciFi.Farm>
2019-01-07 18:52:23 -0700

    Actually fix the migrations. Use pg_isready rather than a time. It can take a surprisingly long time for the server to come up.

--- shell-migrations/20180726000000-install-configs-migrate.sh
+++ shell-migrations/20180726000000-install-configs-migrate.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 
-cd /var/lib/postgresql/data
-while [ ! -f pg_hba.conf ];
+until pg_isready -q
 do
-    echo "Waiting for postgresq.conf"
-    sleep 1
+    echo "Waiting for server to come up."
+    sleep 5
 done
 
-# This allows postgres' initialization to execute first. Then this migration gets applied.
-sleep 5
-
+cd /var/lib/postgresql/data
 mv pg_hba.conf pg_hba.conf.orig
 mv pg_ident.conf pg_ident.conf.orig
 mv postgresql.conf postgresql.conf.orig
 ln -s ../pg_ident.conf .
 ln -s ../postgresql.conf .
 ln -s ../pg_hba.conf .
+
+# Allow enough time for the DB to be created. 
+sleep 15
 
 force_reboot=1
 export force_reboot
